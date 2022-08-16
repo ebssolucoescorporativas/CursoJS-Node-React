fbc5ea36aacde835afcc4c1b4379d6b23271cd36
Eduardo Bueno <eduardo@ebssolucoescorporativas.com.br>
2022-08-15 21:41:00 -0400

    first commit

---
+++ aula3/exercicio.js
@@ -0,0 +1,1 @@
+console.log(`Meu nome é "Eduardo Bueno". Estou aprendendo JavaScript às`, 10, 'da manhã.');
---
+++ aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Eduardo "Bueno"'); // String = Texto
+console.log("Eduardo 'Bueno'");
+console.log(`'Eduardo' "Bueno"`);
+
+console.log(35, 12.45, 'Eduardo Bueno de novo'); // Number = Número
